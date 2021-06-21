b7d24260a5a04de714c26101561387c27326b71b
Jevin Sweval <jevinsweval@gmail.com>
2021-06-21 14:48:17 -0700

    still not enough, as usual

--- litex/build/sim/core/modules/ethernet/ethernet.c
+++ litex/build/sim/core/modules/ethernet/ethernet.c
@@ -125,8 +125,9 @@ static int ethernet_start(void *b)
 #include <netinet/if_ether.h>
 
 #define ARP_MIN_LEN (sizeof(ether_header_t) + sizeof(struct ether_arp))
+#define ARP_MIN_LEN_PADDED 60
 
-static void handle_arp_requests(tapcfg_t *tapcfg, const void *buf, size_t len) {
+static void handle_arp_requests(struct session_s *s, const void *buf, size_t len) {
   const uint8_t *data = (const uint8_t*)buf;
   if (len < ARP_MIN_LEN) {
     fprintf(stderr, "bad len. len: %zu min: %zu\n", len, ARP_MIN_LEN);
@@ -144,8 +145,8 @@ static void handle_arp_requests(tapcfg_t *tapcfg, const void *buf, size_t len) {
   }
   
   fprintf(stderr, "got an ARP packet! src: 0x%08x\n", *(uint32_t*)arp_req->arp_spa);
-  uint8_t reply_buf[ARP_MIN_LEN];
-  memcpy(reply_buf, data, sizeof(reply_buf));
+  uint8_t reply_buf[ARP_MIN_LEN_PADDED] = {0};
+  memcpy(reply_buf, data, ARP_MIN_LEN);
   ether_header_t *eth_reply_hdr = (struct ether_header_t*)reply_buf;
   memcpy(eth_reply_hdr->ether_dhost, ehdr->ether_shost, sizeof(macadr));
   memcpy(eth_reply_hdr->ether_shost, macadr, sizeof(macadr)); 
@@ -155,7 +156,17 @@ static void handle_arp_requests(tapcfg_t *tapcfg, const void *buf, size_t len) {
   memcpy(arp_reply->arp_spa, ipadr, sizeof(ipadr));
   memcpy(arp_reply->arp_tha, arp_req->arp_sha, sizeof(macadr));
   memcpy(arp_reply->arp_tpa, arp_req->arp_spa, sizeof(ipadr));
-  tapcfg_write(tapcfg, reply_buf, sizeof(reply_buf));
+  tapcfg_write(s->tapcfg, reply_buf, sizeof(reply_buf));
+  struct eth_packet_s *rep = malloc(sizeof(struct eth_packet_s));
+  memset(rep, 0, sizeof(struct eth_packet_s));
+  rep->len = sizeof(reply_buf);
+  if(!s->ethpack) {
+    s->ethpack = rep;
+  } else {
+    struct eth_packet_s *tep;
+    for(tep=s->ethpack; tep->next; tep=tep->next);
+    tep->next = rep;
+  }
   fprintf(stderr, "eth arp inject write %d\n", (int)sizeof(reply_buf));
 }
 #endif
@@ -348,7 +359,7 @@ static int ethernet_tick(void *sess, uint64_t time_ps)
       fprintf(stderr, "eth write %d\n", (int)s->datalen);
       tapcfg_write(s->tapcfg, s->databuf, s->datalen);
 #ifdef __APPLE__
-      handle_arp_requests(s->tapcfg, s->databuf, s->datalen);
+      handle_arp_requests(s, s->databuf, s->datalen);
 #endif
       s->datalen=0;
     }

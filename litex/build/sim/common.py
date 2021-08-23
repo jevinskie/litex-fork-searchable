c29a0a66568e8351e9beaa34a3b02e05e0dd2c7e
Jevin Sweval <jevinsweval@gmail.com>
2021-08-22 23:00:44 -0700

    add async timers to async clock mux to allow switching of inactive clocks

--- litex/build/sim/common.py
+++ litex/build/sim/common.py
@@ -16,44 +16,79 @@ from litex.build.io import *
 
 class SimAsyncClockMuxImpl(Module):
     def __init__(self, cd_0: ClockDomain, cd_1: ClockDomain, cd_out: ClockDomain, sel: Signal, rst: Signal):
-        clk1_sel_meta = Signal(name_override=f'acm_cd1_{cd_1.name}_clk1_sel_meta')
-        clk1_ff2_q = Signal(name_override=f'acm_cd1_{cd_1.name}_clk1_ff2_q')
+        clk1_sel_meta = Signal(name_override=f'acm_cd1_{cd_1.name}_sel_meta')
+        clk1_en = Signal(name_override=f'acm_cd1_{cd_1.name}_en')
+        clk1_dis_meta = Signal(name_override=f'acm_cd1_{cd_1.name}_dis_meta')
+        clk1_dis = Signal(name_override=f'acm_cd1_{cd_1.name}_dis')
 
-        clk0_sel_meta = Signal(name_override=f'acm_cd0_{cd_0.name}_clk0_sel_meta')
-        clk0_ff2_q = Signal(name_override=f'acm_cd0_{cd_0.name}_clk0_ff2_q')
+        clk0_sel_meta = Signal(name_override=f'acm_cd0_{cd_0.name}_sel_meta')
+        clk0_en = Signal(name_override=f'acm_cd0_{cd_0.name}_en')
+        clk0_dis_meta = Signal(name_override=f'acm_cd0_{cd_0.name}_dis_meta')
+        clk0_dis = Signal(name_override=f'acm_cd0_{cd_0.name}_dis')
 
         self.specials += [
             Instance("GenericDFF", name=f'acm_cd1_{cd_1.name}_ff0',
-                i_d    = sel & ~clk0_ff2_q,
+                i_d    = sel & ~clk0_en,
                 i_clk  = cd_1.clk,
-                i_r    = rst,
+                i_r    = clk1_dis,
                 i_s    = 0,
                 o_q    = clk1_sel_meta
             ),
             Instance("GenericDFF", name=f'acm_cd1_{cd_1.name}_ff1',
                 i_d    = clk1_sel_meta,
                 i_clk  = ~cd_1.clk,
-                i_r    = rst,
+                i_r    = clk1_dis,
                 i_s    = 0,
-                o_q    = clk1_ff2_q
+                o_q    = clk1_en
             ),
             Instance("GenericDFF", name=f'acm_cd0_{cd_0.name}_ff0',
-                i_d    = ~sel & ~clk1_ff2_q,
+                i_d    = ~sel & ~clk1_en,
                 i_clk  = cd_0.clk,
-                i_r    = rst,
+                i_r    = clk0_dis,
                 i_s    = 0,
                 o_q    = clk0_sel_meta
             ),
             Instance("GenericDFF", name=f'acm_cd0_{cd_0.name}_ff1',
                 i_d    = clk0_sel_meta,
                 i_clk  = ~cd_0.clk,
+                i_r    = clk0_dis,
+                i_s    = 0,
+                o_q    = clk0_en
+            ),
+
+            Instance("GenericDFF", name=f'acm_cd1_{cd_1.name}_tmr_ff0',
+                i_d    = ~sel,
+                i_clk  = cd_0.clk,
+                i_r    = cd_1.clk,
+                i_s    = 0,
+                o_q    = clk1_dis_meta
+            ),
+            Instance("GenericDFF", name=f'acm_cd1_{cd_1.name}_tmr_ff1',
+                i_d    = clk1_dis_meta,
+                i_clk  = cd_0.clk,
+                i_r    = cd_1.clk,
+                i_s    = 0,
+                o_q    = clk1_dis
+            ),
+            Instance("GenericDFF", name=f'acm_cd0_{cd_0.name}_tmr_ff0',
+                i_d    = sel,
+                i_clk  = cd_1.clk,
                 i_r    = rst,
                 i_s    = 0,
-                o_q    = clk0_ff2_q
-            )
+                o_q    = clk0_dis_meta
+            ),
+            Instance("GenericDFF", name=f'acm_cd0_{cd_0.name}_tmr_ff1',
+                i_d    = clk0_dis_meta,
+                i_clk  = cd_1.clk,
+                i_r    = rst,
+                i_s    = 0,
+                o_q    = clk0_dis
+            ),
+
+
         ]
 
-        self.comb += cd_out.clk.eq((cd_1.clk & clk1_ff2_q) | (cd_0.clk & clk0_ff2_q))
+        self.comb += cd_out.clk.eq((cd_1.clk & clk1_en) | (cd_0.clk & clk0_en))
 
 
 class SimAsyncClockMux:

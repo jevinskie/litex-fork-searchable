69d0e3d0cb67c0f602d900cc5b75935ceb51bc62
Jevin Sweval <jevinsweval@gmail.com>
2021-09-07 18:47:57 -0700

    ok this one really works but needs CS

--- litex/soc/cores/spi.py
+++ litex/soc/cores/spi.py
@@ -298,22 +298,22 @@ class SPIMasterStreamer(Module):
 
         self.idle_flag = Signal()
         self.xfer_flag = Signal()
-        self.cs        = Signal()
-        self.last_byte = Signal()
+        # self.cs        = Signal()
+        # self.last_byte = Signal()
 
-        self.comb += self.cs.eq(1)
+        # self.comb += self.cs.eq(1)
 
         # Control FSM ------------------------------------------------------------------------------
         self.submodules.fsm = fsm = FSM(reset_state="IDLE")
         fsm.act("IDLE",
             self.idle_flag.eq(1),
             self.sink.ready.eq(1),
-            If(self.sink.first,
-                NextValue(self.cs, 0),
-            ),
-            If(self.sink.last,
-               NextValue(self.last_byte, 1),
-            ),
+            # If(self.sink.first,
+            #     NextValue(self.cs, 0),
+            # ),
+            # If(self.sink.last,
+            #    NextValue(self.last_byte, 1),
+            # ),
             If(self.sink.valid,
                 self.master.start.eq(1),
                 NextState("XFER")

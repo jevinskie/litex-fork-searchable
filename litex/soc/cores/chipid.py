ede4e0e4eeeaeae94c4b482b43c8edad4bc0c0f0
Jevin Sweval <jevinsweval@gmail.com>
2022-05-22 18:33:51 -0700

    seems to work, will try to tweak

--- litex/soc/cores/chipid.py
+++ litex/soc/cores/chipid.py
@@ -13,42 +13,42 @@ from litex.soc.interconnect.csr import *
 
 class AlteraChipID(Module, AutoCSR):
     def __init__(self):
-        self.regout   = CSRStatus()
-        self.chip_id  = CSRStatus(64)
+        self.regout   = Signal()
+        self.chip_id  = CSRStatus(64, read_only=True)
         self.valid    = CSRStatus()
-        self.go       = CSRStorage()
         self.shiftnld = Signal()
         self.done     = Signal()
 
-        self.count = count = Signal(bits_for(65), reset=65)
+        n_cycles = 65
+        self.count = count = Signal(bits_for(n_cycles), reset=n_cycles)
         self.comb += self.done.eq(count == 0)
         self.sync += If(~self.done, count.eq(count - 1))
 
         self.sync += If(self.shiftnld,
-            self.chip_id.status.eq(Cat(self.chip_id.status[1:], self.regout.status))
+            self.chip_id.status.eq(Cat(self.chip_id.status[1:], self.regout))
         )
 
         self.comb += [
             self.valid.status.eq(self.done),
-            self.shiftnld.eq(~self.done & self.count != self.count.reset),
+            # must pulse shiftnld low at least one cycle
+            self.shiftnld.eq(~self.done & (self.count != self.count.reset))
         ]
 
         self.specials += Instance("fiftyfivenm_chipidblock", "chipid",
             i_clk      = ClockSignal("sys"),
             i_shiftnld = self.shiftnld,
-            o_regout   = self.regout.status,
+            o_regout   = self.regout,
         )
 
 # For verification, delete before merge
 class AlteraChipIDIP(Module, AutoCSR):
     def __init__(self, platform):
-        self.reset = CSRStorage()
-        self.chip_id = CSRStatus(64)
+        self.chip_id = CSRStatus(64, read_only=True)
         self.valid   = CSRStatus()
 
         self.specials += Instance("altchip_id", "chipid",
             i_clkin      = ClockSignal("sys"),
-            i_reset      = self.reset.storage,
+            i_reset      = ResetSignal("sys"),
             o_data_valid = self.valid.status,
             o_chip_id    = self.chip_id.status,
             p_DEVICE_FAMILY = "MAX 10"

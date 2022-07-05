0325a597a7f2e7e9f9e1bc131c9888498e1e8d38
Jevin Sweval <jevinsweval@gmail.com>
2022-07-05 14:18:38 -0700

    wip

--- litex/build/sim/core/vpi.cpp
+++ litex/build/sim/core/vpi.cpp
@@ -10,23 +10,16 @@
 
 #define UNUSED(x) ((void)(x))
 
+static s_vpi_time nextsimtime{.type = vpiSimTime};
+
 extern "C" int litex_sim_main(int argc, const char *argv[]);
 
-struct Vsim {
-    uint8_t foo;
-    void eval(){
-        // printf(".");
-    }
-};
-
-uint64_t main_time = 0;
-Vsim *g_sim        = nullptr;
-bool finished;
+static uint64_t main_time;
+static bool finished;
 
 void litex_sim_eval(void *vsim, uint64_t time_ps) {
-    Vsim *sim = (Vsim *)vsim;
-    sim->eval();
-    main_time = time_ps;
+    UNUSED(vsim);
+    UNUSED(time_ps);
 }
 
 void litex_sim_init_cmdargs(int argc, const char *argv[]) {
@@ -60,12 +53,34 @@ static int end_of_sim_cb(t_cb_data *cbd) {
     return 0;
 }
 
+static void register_next_time_cb();
+
+static int next_time_cb(t_cb_data *cbd) {
+    main_time = ((uint64_t)cbd->time->high << 32) | cbd->time->low;
+    register_next_time_cb();
+    return 0;
+}
+
+static void register_next_time_cb() {
+    s_cb_data nt_cbd{.reason = cbNextSimTime, .cb_rtn = next_time_cb, .time = &nextsimtime};
+    auto nt_cb = vpi_register_cb(&nt_cbd);
+    assert(nt_cb && vpi_free_object(nt_cb));
+}
+
 static int end_of_compile_cb(t_cb_data *cbd) {
     UNUSED(cbd);
+
+    register_next_time_cb();
+    // VPI doesn't callback for time 0 so do it ourself
+    s_vpi_time t0{};
+    s_cb_data cbt0{.time = &t0};
+    next_time_cb(&cbt0);
+
     s_cb_data eos_cbd{.reason = cbEndOfSimulation, .cb_rtn = end_of_sim_cb};
     auto eos_cb = vpi_register_cb(&eos_cbd);
     assert(eos_cb && vpi_free_object(eos_cb));
-    const char *argv[] = {STR(TOPLEVEL), NULL};
+    
+    const char *argv[] = {STR(TOPLEVEL), nullptr};
     litex_sim_main(1, argv);
     return 0;
 }
@@ -77,5 +92,5 @@ static void litex_register() {
 
 void (*vlog_startup_routines[])() = {
     litex_register,
-    NULL,
+    nullptr,
 };
